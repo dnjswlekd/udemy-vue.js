fdcbb16944d588e282bce8120b8308426c2d2206
dnjswlekd <wj8014@gmail.com>
2023-08-14 23:22:39 +0900

    ref 활용하기

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -10,12 +10,25 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value
+      // console.dir(this.$refs.userText)
     },
   },
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  template: `<p>{{ favoritMeal }}</p>`,
+  data(){
+    return {
+      favoritMeal: 'chicken'
+    }
+  }
+})
+
+app2.mount('#app2');
 
 //.....
 const data = {
