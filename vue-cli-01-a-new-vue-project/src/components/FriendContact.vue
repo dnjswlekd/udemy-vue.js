6e0a248e4e0da1ca63dcdac9e520e57f82c4465a
dnjswlekd <wj8014@gmail.com>
2023-08-29 00:05:47 +0900

    vue 프로젝트 설치(i+ssue)
    homebrew issue (설치안되어 있었음),
    node version (다운그레이드;;;;)
    아무튼 오류 개많아서 짜증났음 으악으가아아아아아아악!!!!!!!!!!!!!!!

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">{{detailsAreVisible? 'Hide' : 'Show'}} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ friend.phone }}</li>
+        <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    data() {
+    return {
+      detailsAreVisible: false,
+      friend:
+        {
+          id: 'menuel',
+          name: 'Menuel Lorenz',
+          phone: '010 5275 2671',
+          email: 'wj8014@gmail.com',
+        },
+      }
+    },
+    methods: {
+      toggleDetails(){
+        this.detailsAreVisible = !this.detailsAreVisible;
+      }
+    }
+  }
+</script>
