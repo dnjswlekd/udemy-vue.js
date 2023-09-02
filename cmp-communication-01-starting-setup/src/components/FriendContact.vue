f082828695dad56d612c785305d2d72542abe057
dnjswlekd <wj8014@gmail.com>
2023-09-02 23:50:16 +0900

    v-bind(동적 프로퍼티 값 사용하기)

--- cmp-communication-01-starting-setup/src/components/FriendContact.vue
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
   <li>
-    <h2>{{ name }}</h2>
+    <h2>{{ name }} {{ friendIsFavorite? '(favorite)' : '' }}</h2>
+    <button @click="toggleFavorite">isFavorite</button>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
     <ul v-if="detailsAreVisible">
       <li>
@@ -17,11 +18,34 @@
 
 <script>
 export default {
-  props: [
-    'name',
-    'phoneNumber',
-    'emailAddress'
-  ],
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress:{
+      type: String,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: true,
+      default: false,
+      // validator: function(value){
+      //   return value === '1' || value === '0';
+      // }
+    }
+  },
+  // props: [
+  //   'name',
+  //   'phoneNumber',
+  //   'emailAddress',
+  //   'isFavorite'
+  // ],
   data() {
     return {
       detailsAreVisible: false,
@@ -31,11 +55,15 @@ export default {
         phone: "0123 45678 90",
         email: "manuel@localhost.com",
       },
+      friendIsFavorite: this.isFavorite
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(){
+      this.friendIsFavorite = !this.friendIsFavorite
     }
   }
 };
