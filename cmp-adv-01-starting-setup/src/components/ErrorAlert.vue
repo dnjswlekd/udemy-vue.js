f871d04714e44b6ca1f758993dcc95189a4244ca
dnjswlekd <wj8014@gmail.com>
2023-09-23 20:41:31 +0900

    slot을 사용해서 errorAlert 띄우기

---
+++ cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,23 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  dialog {
+    position: absolute;
+    margin: 0 auto;
+    top: 20vh;
+    width: 40%;
+    padding: 1rem;
+    background: #fff;
+    box-shadow: 0 8px 2px 0 rgba(0, 0, 0, 0.26);
+  }
+</style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,13 +1,39 @@
 <template>
   <div>
     <h2>ManageGoals</h2>
-    <input type="text">
+    <input type="text" ref="goal">
+    <button @click="setGoal">setGoal</button>
+    <error-alert v-if="inputisInvaild">
+      <h2>Input is invaild!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="cofirmError">Okay!</button>
+    </error-alert>
   </div>
 </template>
 
 <script>
+import ErrorAlert from './ErrorAlert.vue';
   export default {
-    
+    components: {
+      ErrorAlert
+    },
+    data(){
+      return {
+        inputisInvaild: false
+      }
+    },
+    methods: {
+      setGoal(){
+        const enteredValue = this.$refs.goal.value;
+        if (enteredValue === '') {
+          // alert('Input must be empty!')
+          this.inputisInvaild = true
+        }
+      },
+      cofirmError(){
+        this.inputisInvaild = false
+      }
+    }
   }
 </script>
 
