998794cc2253d6b9f845d6026f00061ddeb7ada8
dnjswlekd <wj8014@gmail.com>
2023-09-23 20:00:47 +0900

    동적 컴포넌트

--- cmp-adv-01-starting-setup/src/App.vue
+++ cmp-adv-01-starting-setup/src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{ slotProps.item }}</h2>
-      <p>{{ slotProps['anotherProp'] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">ActiveGoals</button>
+    <button @click="setSelectedComponent('manage-goals')">ManageGoals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from '@/components/CourseGoals'
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from '@/components/CourseGoals'
+import ActiveGoals from '@/components/ActiveGoals'
+import ManageGoals from '@/components/ManageGoals'
 
 export default {
   components:{
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
@@ -34,8 +35,14 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComponent: 'null'
     };
   },
+  methods: {
+    setSelectedComponent(cmp){
+      this.selectedComponent = cmp;
+    }
+  }
 };
 </script>
 
