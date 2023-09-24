1c0b654eb4b5f2356ee3953780a086ab2b86b4e9
dnjswlekd <wj8014@gmail.com>
2023-09-24 23:29:26 +0900

    component, props, slot, styling

---
+++ prj-cmp-01-starting-setup/src/components/UI/BaseButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button :type="type" :class="mode">
+  <!-- <base-button mode="flat"></base-button> -->
+   <slot></slot>
+  </button>
+</template>
+
+<script>
+  export default {
+    props: ['type', 'mode']
+  }
+</script>
+
+<style>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #3a0061;
+  border: 1px solid #3a0061;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #270041;
+  border-color: #270041;
+}
+
+.flat {
+  background-color: transparent;
+  color: #3a0061;
+  border: none;
+  cursor: pointer;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #edd2ff;
+}
+
+.customBtn {
+  background-color: green;
+  color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.customBtn:hover,
+.customBtn:active {
+  background: rgb(164, 209, 164);
+  border: 1px solid rgb(164, 209, 164);
+}
+</style>
---
+++ prj-cmp-01-starting-setup/src/components/UI/BaseCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  div {
+    padding: 1rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    margin: 2rem auto;
+    max-width: 40rem;
+  }
+
+</style>
